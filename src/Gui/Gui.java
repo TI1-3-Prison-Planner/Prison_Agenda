e18c0e98ba4272e541fedd9f6d001901f8ca9532
midas <midzzflitzz@gmail.com>
2022-02-10 14:20:35 +0100

    git ignore update and start for Gui

---
+++ src/Gui/Gui.java
@@ -0,0 +1,25 @@
+package Gui;
+
+import javafx.application.Application;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane Mainpane = new BorderPane();
+        TabPane tabPane = new TabPane();
+        Tab tableView = new Tab();
+        Tab roster = new Tab();
+
+
+
+    }
+}
