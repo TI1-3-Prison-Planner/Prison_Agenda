7bbc57a4c8aba7d94725f0976c6a69f1b2e97923
Martijn <mclmvanderlinden@gmail.com>
2022-02-17 13:02:37 +0100

    Quick changes GUI Code
    Reformatted code by style guide

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -25,7 +25,6 @@ import static Gui.AgendaEditor.Editor.ADD;
 
 /**
  * Author:Midas
- *
  */
 public class Gui extends Application {
 
@@ -48,17 +47,17 @@ public class Gui extends Application {
 
     }
 
-    public TabPane getTabPane(){
+    public TabPane getTabPane() {
         TabPane tabPane = new TabPane();
         tabPane.setSide(Side.LEFT);
-        tabPane.getTabs().addAll(getRosterViewTab(),getTableViewRosterTab());
+        tabPane.getTabs().addAll(getRosterViewTab(), getTableViewRosterTab());
 
         return tabPane;
     }
 
-    public Tab getRosterViewTab(){
+    public Tab getRosterViewTab() {
 
-        Tab rosterTab=new Tab();
+        Tab rosterTab = new Tab();
         rosterTab.setText("roster");
         ScrollPane scrollPane = new ScrollPane(getCanvas());
         rosterTab.setContent(scrollPane);
@@ -66,57 +65,59 @@ public class Gui extends Application {
         return rosterTab;
     }
 
-    public Tab getTableViewRosterTab(){
+    public Tab getTableViewRosterTab() {
         Tab tableViewRoster = new Tab();
         tableViewRoster.setText("tableview");
         tableViewRoster.setContent(getTableViewRoster());
         return tableViewRoster;
     }
 
-    public Canvas getCanvas(){
+    public Canvas getCanvas() {
         Canvas canvas = new Canvas();
-        canvas.setOnMouseClicked(e->{drawBlock(new FXGraphics2D(canvas.getGraphicsContext2D()),e);
-
+        canvas.setOnMouseClicked(e -> {
+            drawBlock(new FXGraphics2D(canvas.getGraphicsContext2D()), e);
         });
+
         canvas.setHeight(1500);
         canvas.setWidth(1500);
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()),canvas.getWidth(),canvas.getHeight());
+        draw(new FXGraphics2D(canvas.getGraphicsContext2D()), canvas.getWidth(), canvas.getHeight());
         return canvas;
     }
 
-    public TableView getTableViewRoster(){
+    public TableView getTableViewRoster() {
         TableView tableView = new TableView();
         return tableView;
     }
 
-    public HBox topBox(){
-        HBox topBox= new HBox();
+    public HBox topBox() {
+        HBox topBox = new HBox();
         Label New = new Label("new");
 
         topBox.getChildren().add(New);
         return topBox;
     }
 
-    public void draw(FXGraphics2D graphics,double width,double height){
-        int hour=0;
+    public void draw(FXGraphics2D graphics, double width, double height) {
+        int hour = 0;
         int minute = 0;
-       for (int i = 0;i<width;i+=100){
+        for (int i = 0; i < width; i += 100) {
 
-           graphics.draw(new Line2D.Double(i,height,i,0));
+            graphics.draw(new Line2D.Double(i, height, i, 0));
 
-       }
-       for (int i = 0;i<height;i+=60){
-           graphics.draw(new Line2D.Double(0,i,100,i));
+        }
+        for (int i = 0; i < height; i += 60) {
+            graphics.draw(new Line2D.Double(0, i, 100, i));
 
-           if(i > 1) {
-               graphics.drawString(hour + ":00 - "+(hour+1)+":00", 10, i - 25);
-               hour++;
-           }
+            if (i > 1) {
+                graphics.drawString(hour + ":00 - " + (hour + 1) + ":00", 10, i - 25);
+                hour++;
+            }
 
-       }
+        }
 
     }
-    public void drawBlock(FXGraphics2D g, MouseEvent e){
+
+    public void drawBlock(FXGraphics2D g, MouseEvent e) {
 
         AgendaEditor add = new AgendaEditor(ADD);
         add.start(new Stage());
@@ -126,12 +127,7 @@ public class Gui extends Application {
 //        g.draw(new Rectangle2D.Double(e.getX()-e.getX()%50,e.getY(),50,50));
 
 
-
-
-
-
     }
-
 
 
 }
