06b194d7d2323a839f039f660e08775925df5b91
Moustapha Azaimi <m.azaimi@student.avans.nl>
2022-03-31 15:43:54 +0200

    it sorta works

--- src/Simulatie/Main.java
+++ src/Simulatie/Main.java
@@ -13,6 +13,7 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 public class Main extends Application {
@@ -22,35 +23,35 @@ public class Main extends Application {
 
 
     private Camera camera;
+
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
         javafx.scene.control.Label mouseX = new javafx.scene.control.Label();
         javafx.scene.control.Label mouseY = new javafx.scene.control.Label();
         HBox hBox = new HBox();
-        hBox.getChildren().addAll(mouseX,mouseY);
+        hBox.getChildren().addAll(mouseX, mouseY);
         mainPane.setTop(hBox);
 
 
-        canvas = new ResizableCanvas(g->draw(g),mainPane);
-
+        canvas = new ResizableCanvas(g -> draw(g), mainPane);
 
 
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
-        camera = new Camera(canvas,g -> draw(g),g2d);
+        camera = new Camera(canvas, g -> draw(g), g2d);
         new AnimationTimer() {
             long last = -1;
+
             @Override
             public void handle(long now) {
-                if(last == -1)
+                if (last == -1)
                     last = now;
                 update((now - last) / 1000000000.0);
                 last = now;
                 draw(g2d);
             }
         }.start();
-
 
 
         stage.setScene(new Scene(mainPane));
@@ -63,10 +64,10 @@ public class Main extends Application {
 
         canvas.setOnMouseMoved(event -> {
             try {
-                Point2D clickPos = camera.getTransform((int)this.canvas.getWidth(), (int)this.canvas.getHeight()).inverseTransform(new Point2D.Double(event.getX(), event.getY()), null);
-                mouseX.setText("x: "+  (int)  clickPos.getX());
-                mouseY.setText("Y: "+  (int) clickPos.getY());
-                for(Visitor visitor : this.visitors) {
+                Point2D clickPos = camera.getTransform((int) this.canvas.getWidth(), (int) this.canvas.getHeight()).inverseTransform(new Point2D.Double(event.getX(), event.getY()), null);
+                mouseX.setText("x: " + (int) clickPos.getX());
+                mouseY.setText("Y: " + (int) clickPos.getY());
+                for (Visitor visitor : this.visitors) {
                     visitor.setTarget(clickPos);
                 }
             } catch (NoninvertibleTransformException e) {
@@ -78,20 +79,19 @@ public class Main extends Application {
 
     }
 
-    private  ArrayList<Visitor> visitors;
+    private ArrayList<Visitor> visitors;
     private double timer;
-    public void init()
-    {
+
+    public void init() {
         this.visitors = new ArrayList<>();
         maps = new Map();
         for (String s : maps.locationObjects.keySet()) {
 //            System.out.println("hoi");
-             Visitor visitor = new Visitor(new Point2D.Double(maps.locationObjects.get(s).getPosition().getX()+maps.locationObjects.get(s).getSize().getX()/2,
-                                                                maps.locationObjects.get(s).getPosition().getY()+maps.locationObjects.get(s).getSize().getY()/2), 0,maps);
-            if(!visitor.checkCollision(this.visitors))
-            {
-                this.visitors.add(visitor);
-            }
+            Visitor visitor = new Visitor(new Point2D.Double(maps.locationObjects.get(s).getPosition().getX() + maps.locationObjects.get(s).getSize().getX() / 2,
+                    maps.locationObjects.get(s).getPosition().getY() + maps.locationObjects.get(s).getSize().getY() / 2), 0, maps, true);
+            this.visitors.add(visitor);
+            break;
+
         }
 
 
@@ -108,24 +108,23 @@ public class Main extends Application {
     }
 
 
-
-
-
-    public void draw(FXGraphics2D g)
-    {
-        g.scale(0.5,0.5);
+    public void draw(FXGraphics2D g) {
+        g.scale(0.5, 0.5);
         g.setTransform(new AffineTransform());
         g.setBackground(Color.black);
-        g.clearRect(0,0,(int)canvas.getWidth(), (int)canvas.getHeight());
+        g.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
         AffineTransform originalTransform = g.getTransform();
-        g.setTransform(camera.getTransform((int)canvas.getWidth(),(int)canvas.getHeight()));
+        g.setTransform(camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()));
 
-        g.setTransform(camera.getTransform((int)canvas.getWidth(),(int)canvas.getHeight()));
-       maps.draw(g,camera);
-        for(Visitor visitor : this.visitors) {
+        g.setTransform(camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()));
+        maps.draw(g, camera);
+        for (Visitor visitor : this.visitors) {
             visitor.draw(g);
+            //todo: implementeer connectie met agendalocaties
+
         }
-       g.setTransform(originalTransform);
+
+        g.setTransform(originalTransform);
 
 
     }
@@ -133,30 +132,22 @@ public class Main extends Application {
     public void update(double deltaTime) {
 
 //        maps.setTx(camera.getTransform((int)canvas.getWidth(),(int)canvas.getHeight()).);
-        timer+=deltaTime;
-        if(timer > 10)
-        {
+        timer += deltaTime;
+        if (timer > 10) {
             timer -= 10;
-            for(Visitor visitor : this.visitors)
-            {
-                visitor.setTarget(new Point2D.Double(Math.random()*4800, Math.random()*4800));
+            for (Visitor visitor : this.visitors) {
+                visitor.setTarget(new Point2D.Double(Math.random() * 4800, Math.random() * 4800));
             }
         }
 
 
-        for(Visitor visitor : this.visitors) {
+        for (Visitor visitor : this.visitors) {
             visitor.update(this.visitors);
         }
     }
 
 
-
-
-
-
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Main.class);
     }
 
--- src/Simulatie/Visitor.java
+++ src/Simulatie/Visitor.java
@@ -10,64 +10,67 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Visitor
-{
+public class Visitor {
     private Point2D position;
+
     private double angle;
-    private ArrayList<BufferedImage> sprites;
+    private ArrayList<BufferedImage> inmateSprites;
+    private ArrayList<BufferedImage> guardSprites;
+    private boolean isGuard;
     private double speed;
     private double frame;
     private Point2D target;
     private static double rotationSpeed = 0.1;
     private Map map;
 
-
-    public Visitor(Point2D position, double angle,Map map)
-    {
+    public Visitor(Point2D position, double angle, Map map, boolean isGuard) {
         this.position = position;
         this.angle = angle;
-        this.speed = 1+5 * Math.random();
+        this.speed = 1 + 5 * Math.random();
         this.target = position;
-        this.frame = Math.random()*10;
+        this.frame = Math.random() * 10;
         this.map = map;
+        this.isGuard = isGuard;
 
-        this.sprites = new ArrayList<>();
-        try
-        {
-            BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/images/npc.png"));
-            int w = image.getWidth()/2;
-            int h = image.getHeight()/3;
-            for(int y = 0; y < 3; y++) {
-                for(int x = 0; x < 2; x++) {
-                    this.sprites.add(image.getSubimage(x * w, y * h, w,h));
+        this.inmateSprites = new ArrayList<>();
+        this.guardSprites = new ArrayList<>();
+        try {
+
+            BufferedImage inmateImage = ImageIO.read(getClass().getResourceAsStream("/images/inmate.png"));
+            BufferedImage guardImage = ImageIO.read(getClass().getResourceAsStream("/images/guard.png"));
+            int w = 64;
+            int h = 64;
+            for (int y = 0; y < 1; y++) {
+                for (int x = 0; x < 3; x++) {
+                    this.inmateSprites.add(inmateImage.getSubimage(x * w, y * h, w, h));
+                    this.guardSprites.add(guardImage.getSubimage(x * w, y * h, w, h));
                 }
+
             }
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
 
-    public void update(ArrayList<Visitor> visitors)
-    {
-        if(target.distanceSq(position) < 32)
+    public void update(ArrayList<Visitor> visitors) {
+        if (target.distanceSq(position) < 32)
             return;
-        
+
         double targetAngle = Math.atan2(this.target.getY() - this.position.getY(), this.target.getX() - this.position.getX());
         double rotation = targetAngle - this.angle;
-        while(rotation < -Math.PI) {
-            rotation += 2*Math.PI;
+        while (rotation < -Math.PI) {
+            rotation += 2 * Math.PI;
         }
-        while(rotation > Math.PI) {
-            rotation -= 2*Math.PI;
+        while (rotation > Math.PI) {
+            rotation -= 2 * Math.PI;
         }
 
         double oldAngle = this.angle;
-        if(rotation < -rotationSpeed) {
+        if (rotation < -rotationSpeed) {
             this.angle -= rotationSpeed;
-        } else if(rotation > rotationSpeed) {
+        } else if (rotation > rotationSpeed) {
             this.angle += rotationSpeed;
         } else {
             this.angle = targetAngle;
@@ -82,53 +85,76 @@ public class Visitor
         hasCollision = hasCollision || checkCollision(visitors);
 
 
-        if(hasCollision) {
+        if (hasCollision) {
             this.position = oldPosition;
-            this.frame = 0;
             this.angle = oldAngle + rotationSpeed;
+        }
+
+        double degrees = Math.toDegrees(this.angle);
+        if (degrees < 0) {
+            degrees += 360;
+        }
+        degrees %= 360;
+        if (degrees >= 45 && degrees < 135) {
+            this.frame = 0;
+        } else if (degrees >= 135 && degrees < 225) {
+            this.frame = 2;
+        } else if (degrees >= 225 && degrees < 315) {
+            this.frame = 1;
         } else {
-            this.frame += 0.1;
+            this.frame = 2;
         }
     }
 
-    public boolean checkCollision(ArrayList<Visitor> visitors)
-    {
+    public boolean checkCollision(ArrayList<Visitor> visitors) {
 
         boolean hasCollision = false;
-        if(map.layers.get(1)[(int) Math.max(0,this.position.getY()/32) ][(int) Math.max(0,this.position.getX()/32)] ==-1 ){
+        if (map.layers.get(1)[(int) Math.max(0, this.position.getY() / 32)][(int) Math.max(0, this.position.getX() / 32)] == -1) {
             hasCollision = true;
         }
 
-        for(Visitor visitor : visitors) {
-            if(visitor != this) {
-                if(visitor.position.distanceSq(position) < 64*64) {
-                    hasCollision = true;
-                }
-            }
-        }
+//        for(Visitor visitor : visitors) {
+//            if(visitor != this) {
+//                if(visitor.position.distanceSq(position) < 64*64) {
+//                    hasCollision = true;
+//                }
+//            }
+//        }
         return hasCollision;
     }
 
-    public void draw(Graphics2D g2d)
-    {
-        int centerX = sprites.get(0).getWidth()/2;
-        int centerY = sprites.get(0).getHeight()/2;
+    public void draw(Graphics2D g2d) {
+        int centerX = guardSprites.get(0).getWidth() / 2;
+        int centerY = guardSprites.get(0).getHeight() / 2;
         AffineTransform tx = new AffineTransform();
         tx.translate(position.getX() - centerX, position.getY() - centerY);
-        tx.rotate(angle + Math.PI/2, centerX, centerY);
         tx.translate(0, 20);
 
+        ArrayList<BufferedImage> sprites;
+        if (isGuard) {
+            sprites = guardSprites;
+        } else {
+            sprites = inmateSprites;
+        }
 
-        g2d.drawImage(this.sprites.get((int)Math.floor(frame) % this.sprites.size()), tx, null);
+
+        g2d.drawImage(sprites.get((int) Math.floor(frame) % sprites.size()), tx, null);
 
 
         g2d.setColor(Color.white);
-        g2d.draw(new Ellipse2D.Double(position.getX()-32, position.getY()-32, 64, 64));
+        g2d.draw(new Ellipse2D.Double(position.getX() - 32, position.getY() - 32, 64, 64));
 //        g2d.draw(new Line2D.Double(position, target));
     }
 
-    public void setTarget(Point2D newTarget)
-    {
+    public void setTarget(Point2D newTarget) {
         this.target = newTarget;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public Point2D getPosition() {
+        return position;
     }
 }
